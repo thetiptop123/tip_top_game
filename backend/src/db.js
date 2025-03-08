8c554e3f40d58c8e926a035a697743bb307063a1
mouad-sellak <mouaadsellak123@gmail.com>
2025-03-08 07:26:02 +0100

    Fix 3T-unYHWJRX: mongodb connexion - done

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,18 +1,17 @@
-// db.js
-
+// src/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // process.env.MONGO_URI vient de .env (chargé plus tard par server.js)
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+      // Ces options sont désormais dépréciées avec le driver v4 :
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
     console.log('MongoDB connected...');
   } catch (err) {
     console.error('Erreur de connexion à MongoDB :', err);
-    process.exit(1); // Arrête l'app si la connexion échoue
+    process.exit(1);
   }
 };
 
