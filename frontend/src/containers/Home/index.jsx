3a5210fbcc1b816d5b17462fc780d25707e3e38f
laminaacharaf <atypikhouse2023@gmail.com>
2025-03-11 15:32:09 +0100

    3T-Vkjs7P3K:Tout est done

--- frontend/src/containers/Home/index.jsx
+++ frontend/src/containers/Home/index.jsx
@@ -2,14 +2,14 @@ import Container from "../../components/Container";
 import GoGameButton from "../../components/GameButton";
 
 const Lots = () => (
-  <div className="lots">
+  <div >
     <h2 style={{paddingLeft:10}}>Découvrez les lots à gagner</h2>
     <div style={{
       display: 'flex',
       justifyContent: 'space-around',
       flexWrap: 'wrap',
     }}>
-      <div className="lot" style={{
+      <div style={{
         margin: '10px',
         textAlign: 'center',
       }}>
@@ -25,7 +25,7 @@ const Lots = () => (
           color: '#333',
         }}>Lot 1 : Infuseur à thé</p>
       </div>
-      <div className="lot" style={{
+      <div style={{
         margin: '10px',
         textAlign: 'center',
       }}>
@@ -41,7 +41,7 @@ const Lots = () => (
           color: '#333',
         }}>Lot 2 : Boite de 100g d’un thé détox ou d’infusion</p>
       </div>
-      <div className="lot" style={{
+      <div  style={{
         margin: '10px',
         textAlign: 'center',
       }}>
@@ -57,7 +57,7 @@ const Lots = () => (
           color: '#333',
         }}>Lot 3 : Boite de 100g d’un thé signature</p>
       </div>
-      <div className="lot" style={{
+      <div style={{
         margin: '10px',
         textAlign: 'center',
       }}>
@@ -73,7 +73,7 @@ const Lots = () => (
           color: '#333',
         }}>Lot 4 : Coffret découverte d’une valeur de 39€</p>
       </div>
-      <div className="lot" style={{
+      <div  style={{
         margin: '10px',
         textAlign: 'center',
       }}>
@@ -102,12 +102,9 @@ export default function Home() {
       justifyContent: "space-between",
     }}>
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <center>
-          <h1 style={{ color: '#39590A', fontSize: '900' }}>
-            Participez au jeu-concours Thé Tip Top 100 % gagnant avec différents cadeaux exclusivement réservés pour vous!
-          </h1>
-        </center>
-        
+        <h1 style={{ color: '#39590A', fontSize: '900', marginLeft:50 }}>
+          Participez au jeu-concours Thé Tip Top 100 % gagnant avec différents cadeaux exclusivement réservés pour vous!
+        </h1>
         <img src="/img_home.png" alt="jeu-concours" style={{ display: "flex", flexDirection: "center", marginBottom: '20px',borderRadius: '20px' }} />
         <div style={{ width: 825, textAlign: 'center', marginBottom: '20px' }}>
           The Tip Top vous propose un jeu-concours pour vous fidéliser et répondre
