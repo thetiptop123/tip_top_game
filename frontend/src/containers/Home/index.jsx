90002b41e223cc3cae337706dda6cd4efeb2cbb6
laminaacharaf <atypikhouse2023@gmail.com>
2025-03-11 13:20:45 +0100

    3T-Vkjs7P3K: Modification de la page d'accueil

--- frontend/src/containers/Home/index.jsx
+++ frontend/src/containers/Home/index.jsx
@@ -12,10 +12,10 @@ export default function Home() {
       }}
     >
       <div>
-        <img src="/image1_home.png" width={1052} height={252} />
+        <img src="/img_home.png" width={1052} height={252} />
       </div>
       <div>
-        <GoGameButton goTo="profile">Commencer le jeu</GoGameButton>
+        <GoGameButton goTo="profile">Accéder au jeu</GoGameButton>
       </div>
       <div style={{ width: 825 }}>
         The Tip Top vous propose un jeu-concours pour vous fediliser et répondre
@@ -23,6 +23,33 @@ export default function Home() {
         de thé de très grande qualité, elle est parmi les leaders dans le
         domaine. à vous de tenter votre chance.
       </div>
+
+      {/* Présentation des lots */}
+     <div class="lots">
+        <h2>Découvrez les lots à gagner</h2>
+        <div class="lots-container">
+            <div >
+                <img src="/lot1.jpg" alt="Lot 1" />
+                <p>Lot 1 : Infuseur à thé</p>
+            </div>
+            <div >
+                <img src="/lot2.jpg" alt="Lot 2" />
+                <p>Lot 2 : Boite de 100g d’un thé détox ou d’infusion</p>
+            </div>
+            <div >
+                <img src="/Lot3.jpg" alt="Lot 3" />
+                <p>Lot 3 :  Boite de 100g d’un thé signature </p>
+            </div>
+            <div >
+                <img src="/Lot4.jpg" alt="Lot 4" />
+                <p>Lot 4 :  Coffret découverte d’une valeur de 39€</p>
+            </div>
+            <div >
+                <img src="/LOT.jpg" alt="Lot 5" />
+                <p>Lot 5 : Coffret découverte d’une valeur de 69€ </p>
+            </div>
+        </div>
+    </div>
     </Container>
   );
 }
