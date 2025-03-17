7be38b3b3f896bf7b02d1cf6e3d4138410d2a50e
azolnonix <azizeolougbokiki93@gmail.com>
2025-03-17 01:14:51 +0100

    3T-F9jNuWlS ajout du grand gagnant feature

--- backend/src/routes/recordGameRoute.js
+++ backend/src/routes/recordGameRoute.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 const { authMiddleware } = require("../middlewares/authMiddleware");
 const { userTypeMiddleware } = require("../middlewares/userTypeMiddleware");
-const { recordGameController } = require("../controllers/gameController");
+const { recordGameController , grandTirageController} = require("../controllers/gameController");
 
 
 //register user
 router.post('/jeux', authMiddleware,userTypeMiddleware("client"), recordGameController);
+
+// grand tirage
+router.post('/grandJeu', authMiddleware,userTypeMiddleware("admin"), grandTirageController);
 
 
 
